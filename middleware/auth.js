9ad12050990f7d3f09aff1066937542c68ad1b93
gesarizky <gesarizkynugraha@gmail.com>
2024-07-29 11:46:21 +0700

    edit auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const authenticateJWT = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) return res.status(401).send("Access Denied");
+   const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
     const verified = jwt.verify(token, SECRET_KEY);
